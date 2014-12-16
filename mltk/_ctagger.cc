efdb7efcf9267c9c2e96527d3b4ae52841b86195
Matthew Peters <matt@moz.com>
2014-12-16 15:12:01 -0800

    Move some utilities into a common location

--- mltk/_ctagger.cc
+++ mltk/_ctagger.cc
@@ -7,18 +7,7 @@
 #include <algorithm>
 #include <tr1/functional>
 
-#include "../ext/murmur3.c"
-
-
-uint32_t const SEED = 5;
-
-/// Use murmurhash as a custom has for the string
-uint64_t murmurhash3(const std::string& key)
-{
-    uint64_t ret[2];   // need 128 bits of space to hold result
-    MurmurHash3_x64_128(key.c_str(), key.length(), SEED, ret);
-    return ret[0];
-}
+#include "_utils.cc"
 
 
 /**
@@ -66,32 +55,6 @@ typedef std::map<std::string, std::string> tagmap_in_t;
 
 
 
-//------------------------------------------------------
-// utilities
-std::string normalize(std::string const & word)
-{
-    /**< normalize a word.
-     - All words are lower cased
-     - 4 letter digits are represented as !YEAR
-     - Other digits are represented as !DIGITS
-    */
-    if (word.find("-") != std::string::npos && word != "-")
-        return "!HYPHEN";
-    else if (word.find_first_not_of("0123456789") == std::string::npos &&
-        word.length() == 4)
-        return "!YEAR";
-    else if (std::isdigit(word[0]))
-        return "!DIGITS";
-    else
-    {
-        // lowercase
-        std::string ret;
-        ret.assign(word);
-        std::transform(ret.begin(), ret.end(), ret.begin(), ::tolower);
-        return ret;
-    }
-}
-
 std::string last_n_letters(std::string const & s, int n)
 {
     ///< get the last three letters (or less) of the string
@@ -101,20 +64,6 @@ std::string last_n_letters(std::string const & s, int n)
         return s.substr(0);
 }
 
-
-const std::string SPACE = " ";
-
-std::string join(std::string& s1, std::string& s2)
-{
-    /**< join with a space in the center
-     find the length of the final string, reserve it then append */
-    std::string sout;
-    sout.reserve(s1.length() + s2.length() + 1);
-    sout.append(s1);
-    sout.append(SPACE);
-    sout.append(s2);
-    return sout;
-}
 
 void get_features(std::size_t k,
     std::string const & word,
